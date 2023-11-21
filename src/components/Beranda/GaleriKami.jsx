4a210af33032409d9a98e93ec7bdb94f21b03fed
hilmiyahya19 <hilmiyahya199@gmail.com>
2023-11-21 18:21:51 +0700

    coba7

--- src/components/Beranda/GaleriKami.jsx
+++ src/components/Beranda/GaleriKami.jsx
@@ -1,8 +1,8 @@
 function GaleriKami() {
   return (
     <div>
-      <section className="h-c820">
-        <h1 className="text-customBlue3 text-center text-4xl font-bold mt-10">Galeri Kami</h1>
+      <section className="h-c850">
+        <h1 className="text-customBlue3 text-center text-4xl font-bold mt-14">Galeri Kami</h1>
       </section>
     </div>
   )
